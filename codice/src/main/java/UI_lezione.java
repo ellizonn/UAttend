2bf87a3f6fa9a1e84ea64cf35fcaf421c7c26231
20005068 <20005068@studenti.uniupo.it>
2021-12-15 12:46:44 +0100

    Creazione del branch RF05. Nel .vpp manca la modifica suggerita da Filiberto il 15/12 alle 12.34 su Telegram; è stata corretta la nomenclatura <insegmaneto --> corso>. Nei file sorgente ho aggiunto i metodi 1.x e 2.x del diagramma di sequenza.

--- codice/src/main/java/UI_lezione.java
+++ codice/src/main/java/UI_lezione.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UI_lezione
@@ -11,5 +12,19 @@ public class UI_lezione
 	ui_avv=ui1;
 
 	g_lez=g1;
+    }
+
+    /**
+     * @author: Simone Garau & Filiberto Melis
+     */
+    public void visualizza_elenco_corsi(){
+        ArrayList<corso> elencoCorsi = g_lez.richiesta_elenco_corsi();
+        if(elencoCorsi != null){
+            System.out.println("Selezionare una lezione");
+            for(int i = 0; i < elencoCorsi.size(); i++){
+                System.out.println(String.format("Lezione %d" + elencoCorsi.get(i).toString(), i+1));
+            }
+        }
+            
     }
 }
--- codice/src/main/java/gestore_lezioni.java
+++ codice/src/main/java/gestore_lezioni.java
@@ -7,4 +7,11 @@ class gestore_lezioni
       //autore: Codetta
       db_lez=d1;
     }
+
+    /**
+     * @author: Simone Garau & Filiberto Melis
+     */
+    public ArrayList<corso> richiesta_elenco_corsi(){
+        return db_lez.carica_corsi();
+    }
 }
