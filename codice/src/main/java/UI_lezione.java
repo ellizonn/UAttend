c89ffd9644f9b41108c9c44137353820404ee56f
Filiberto Melis <20035059@studenti.uniupo.it>
2021-12-16 22:42:15 +0100

    implementazione visualizzazione errori e modifica dei form

--- codice/src/main/java/UI_lezione.java
+++ codice/src/main/java/UI_lezione.java
@@ -1,5 +1,4 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -18,11 +17,8 @@ public class UI_lezione {
         g_lez = g1;
     }
 
-    /**
-     * @author: Simone Garau
-     * @author: Filiberto Melis
-     */
     public void visualizza_elenco_corsi() {
+        // autori: Simone Garau, Filiberto Melis
         ArrayList<corso> elencoCorsi = g_lez.richiesta_elenco_corsi();
 
         if (elencoCorsi != null) {
@@ -34,51 +30,149 @@ public class UI_lezione {
     }
 
     public void form_data() {
+        // autori: Simone Garau, Filiberto Melis
         Scanner scanner = new Scanner(System.in);
-        boolean formato = false;
+        String conferma = "annulla";
+        boolean formato;
 
         do {
-            System.out.print("Inserisci data (gg/MM/aaaa): ");
+            System.out.print("Inserire data (gg/MM/aaaa): ");
             String data = scanner.nextLine();
             Pattern p = Pattern.compile("(\\d{2})/(\\d{2})/(\\d{4})");
             Matcher m = p.matcher(data);
 
-            if (m.matches()) {
+            if (formato = m.matches()) {
                 int giorno = Integer.parseInt(m.group(1));
                 int mese = Integer.parseInt(m.group(2));
                 int anno = Integer.parseInt(m.group(3));
-                this.giorno = LocalDate.of(anno, mese, giorno);
-                formato = true;
+                try {
+                    this.giorno = LocalDate.of(anno, mese, giorno);
+                } catch (DateTimeException e) {
+                    System.out.println("ATTENZIONE: data inesistente");
+                    formato = false;
+                }
             } else
-                System.out.println("Formato data errato");
-        }
-        while (!formato);
+                System.out.println("ATTENZIONE: formato data errato");
+
+            if (formato) {
+                System.out.print("Confermi la data (s/n)? ");
+                conferma = scanner.nextLine();
+
+                if (conferma.equals("s") | conferma.equals("S"))
+                    conferma = "conferma";
+                else conferma = "annulla";
+            }
+        } while (!formato || conferma.equals("annulla"));
     }
 
     public void form_orario() {
+        // autori: Simone Garau, Filiberto Melis
         Scanner scanner = new Scanner(System.in);
-        boolean formato = false;
+        String conferma = "annulla";
+        boolean formato;
 
         do {
-            System.out.print("Inserisci orario inizio - orario fine (h24) (HH:mm-HH:mm): ");
+            System.out.print("Inserire orario di inizio e di fine lezione (h24) (HH:mm-HH:mm): ");
             String orario = scanner.nextLine();
-            Pattern p = Pattern.compile("(\\d{2}):(\\d{2}-(\\d{2}):(\\d{2}))");
+            Pattern p = Pattern.compile("(\\d{2}):(\\d{2})-(\\d{2}):(\\d{2})");
             Matcher m = p.matcher(orario);
 
-            if (m.matches()) {
+            if (formato = m.matches()) {
                 int ora_inizio = Integer.parseInt(m.group(1));
                 int minuto_inizio = Integer.parseInt(m.group(2));
                 int ora_fine = Integer.parseInt(m.group(3));
                 int minuto_fine = Integer.parseInt(m.group(4));
-                this.ora_inizio = LocalTime.of(ora_inizio, minuto_inizio);
-                this.ora_fine = LocalTime.of(ora_fine, minuto_fine);
-                formato = true;
+                try {
+                    this.ora_fine = LocalTime.of(ora_fine, minuto_fine);
+                    this.ora_inizio = LocalTime.of(ora_inizio, minuto_inizio);
+                } catch (DateTimeException e) {
+                    System.out.println("ATTENZIONE: orario inesistente");
+                    formato = false;
+                }
             } else
-                System.out.println("Formato ora errato");
+                System.out.println("ATTENZIONE: formato ora errato");
+
+            if (formato) {
+                System.out.print("Confermi l'orario (s/n)? ");
+                conferma = scanner.nextLine();
+
+                if (conferma.equals("s") | conferma.equals("S"))
+                    conferma = "conferma";
+                else conferma = "annulla";
+            }
+        } while (!formato || conferma.equals("annulla"));
+    }
+
+    public void mostra_errore_data(LocalDate data) {
+        // autori: Simone Garau, Filiberto Melis
+        if (data == null)
+            System.out.println("ERRORE: nessuna data inserita");
+        else {
+            if (data.isBefore(LocalDate.now()))
+                System.out.println("ERRORE: la data deve essere successiva alla data odierna");
+            if (data.getDayOfWeek().equals(DayOfWeek.SATURDAY))
+                System.out.println("ERRORE: non ci possono essere lezioni il sabato");
+            if (data.getDayOfWeek().equals(DayOfWeek.SUNDAY))
+                System.out.println("ERRORE: non ci possono essere lezioni la domenica");
+            if (data.getMonth().equals(Month.AUGUST))
+                System.out.println("ERRORE: non ci possono essere lezioni ad agosto");
         }
-        while (!formato);
+        System.out.print("Premi INVIO per conferma");
+        new Scanner(System.in).nextLine();
+    }
+
+    public void mostra_errore_orario(LocalTime ora_inizio, LocalTime ora_fine) {
+        // autori: Simone Garau, Filiberto Melis
+        if (ora_inizio == null || ora_fine == null) {
+            if (ora_inizio == null)
+                System.out.println("ERRORE: nessun orario di inizio inserito");
+            else
+                System.out.println("ERRORE: nessun orario di fine inserito");
+        } else {
+            if (ora_inizio.isAfter(ora_fine) || ora_inizio.isAfter(ora_fine.minusHours(1))) {
+                if (ora_inizio.isAfter(ora_fine))
+                    System.out.println("ERRORE: l'inizio della lezione deve precedere la fine");
+                else
+                    System.out.println("ERRORE: la lezione deve durare almeno un'ora");
+            }
+            if (ora_inizio.getHour() < 9)
+                System.out.println("ERRORE: la lezione non deve iniziare prima delle 9");
+            if (ora_inizio.getHour() > 17)
+                System.out.println("ERRORE: la lezione non deve iniziare dopo le 17");
+            if (ora_fine.getHour() < 10)
+                System.out.println("ERRORE: la lezione non deve finire prima delle 10");
+            if (ora_fine.getHour() > 18)
+                System.out.println("ERRORE: la lezione non deve finire dopo le 18");
+        }
+        System.out.print("Premi INVIO per conferma");
+        new Scanner(System.in).nextLine();
+    }
+
+    public void mostra_errore_aula() {
+        // autori: Simone Garau, Filiberto Melis
+        System.out.println("ERRORE: nessuna aula disponibile");
+        System.out.print("Premi INVIO per conferma");
+        new Scanner(System.in).nextLine();
     }
 
     public void avvio_aggiungi_lezione() {
+        this.form_data();
+        this.form_orario();
+
+        if (!g_lez.verifica_correttezza_data(this.giorno))
+            this.mostra_errore_data(this.giorno);
+        if (!g_lez.verifica_correttezza_orario(this.ora_inizio, this.ora_fine))
+            this.mostra_errore_orario(this.ora_inizio, this.ora_fine);
+    }
+
+    public static void main(String[] args) {
+        DB_avvisi da = new DB_avvisi();
+        gestore_avvisi ga = new gestore_avvisi(da);
+        UI_avviso ua = new UI_avviso(ga);
+        DB_lezioni dl = new DB_lezioni();
+        gestore_lezioni gl = new gestore_lezioni(dl);
+        UI_lezione ul = new UI_lezione(ua, gl);
+
+        ul.avvio_aggiungi_lezione();
     }
 }
--- codice/src/main/java/gestore_lezioni.java
+++ codice/src/main/java/gestore_lezioni.java
@@ -15,10 +15,9 @@ class gestore_lezioni {
 
     /**
      * @return elenco corsi.
-     * @author: Simone Garau
-     * @author: Filiberto Melis
      */
     public ArrayList<corso> richiesta_elenco_corsi() {
+        // autori: Simone Garau, Filiberto Melis
         return db_lez.carica_corsi();
     }
 
@@ -32,17 +31,16 @@ class gestore_lezioni {
 
     /**
      * @return elenco corsi.
-     * @author: Simone Garau
-     * @author: Filiberto Melis
      */
     public boolean verifica_correttezza_orario(LocalTime ora_inizio, LocalTime ora_fine) {
+        // autori: Simone Garau, Filiberto Melis
         return ora_inizio != null
                 && ora_fine != null
                 && ora_inizio.isBefore(ora_fine)
-                && ora_inizio.isBefore(ora_fine.minusHours(1))
+                && !ora_inizio.isAfter(ora_fine.minusHours(1))
                 && ora_inizio.getHour() >= 9
-                && ora_inizio.getHour() <= 18
-                && ora_fine.getHour() >= 9
+                && ora_inizio.getHour() <= 17
+                && ora_fine.getHour() >= 10
                 && ora_fine.getHour() <= 18;
     }
 }
