ad86c9aa452614248028ff5938afc19637874ae2
Francesco Beccuti <20034904@studenti.uniupo.it>
2021-12-16 09:29:27 +0100

    commit di visualizza avvisi recenti nel codice src

--- codice/src/main/java/UI_avviso.java
+++ codice/src/main/java/UI_avviso.java
@@ -1,12 +1,51 @@
+package main.java;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UI_avviso
 {   
-    gestore_avvisi g_avv;
+    public gestore_avvisi g_avv;
 
     public UI_avviso(gestore_avvisi g1)
     {	
 	//autore: Codetta
 	g_avv=g1;
     }
+    
+    public void visualizza_errore() {
+
+		// autore: Beccuti/Iabichino
+    	
+    	System.out.println("Non ci sono avvisi recenti.");
+    	
+    	System.out.println("Premi INVIO per conferma.");
+    	
+    	Scanner scan = new Scanner(System.in);
+    	
+    	scan.nextLine();
+    	
+    	scan.close();
+    	
+    }
+    
+    public void visualizza_elenco() {
+
+		// autore: Beccuti/Iabichino
+    	
+    	ArrayList<avviso> a = g_avv.richiesta_avvisi_recenti();
+    	
+    	if(a.isEmpty()) {
+    		
+    		visualizza_errore();
+    	}
+    	
+    	for(avviso b:a) {
+    		
+    		System.out.println(b.testo+ " "+b.emissione);
+    		
+    	}
+    	
+    }
+    
 }
--- codice/src/main/java/gestore_avvisi.java
+++ codice/src/main/java/gestore_avvisi.java
@@ -1,10 +1,21 @@
-class gestore_avvisi
+package main.java;
+
+import java.util.ArrayList;
+
+public class gestore_avvisi
 {
-    DB_avvisi db_avv;
+    public DB_avvisi db_avv;
 
     public gestore_avvisi(DB_avvisi d1)
     {
       //autore: Codetta
       db_avv=d1;
+    }
+    
+    public ArrayList<avviso> richiesta_avvisi_recenti(){
+
+      // autore: Beccuti/Iabichino
+		
+    	return db_avv.cerca_avvisi();
     }
 }
