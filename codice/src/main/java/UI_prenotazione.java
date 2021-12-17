786dcef04c468e77fbbbbde5a7e4464bf7a127d0
20036782 <20036782@studenti.uniupo.it>
2021-12-17 12:34:42 +0100

    Aggiunta metodi form_prenotazione, mostra_messaggio e controllo_data_e_posti. No test.

--- codice/src/main/java/UI_prenotazione.java
+++ codice/src/main/java/UI_prenotazione.java
@@ -1,12 +1,37 @@
 import java.util.Scanner;
 
 public class UI_prenotazione
-{   
-    gestore_lezioni g_lez;
+{   	
+	gestore_lezioni g_lez;
 
     public UI_prenotazione(gestore_lezioni g1)
     {	
 	//autore: Codetta
 	g_lez=g1;
     }
+    
+    public String form_prenotazione(lezione lez)
+    {
+    	//RF13_prenota_posto
+    	//Autori: Rossari, Marisio
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nHai selezionato la lezione di "+lez.nome_corso+", prevista alle ore "+lez.ora_inizio+" del "+lez.giorno+".\nDigitare 'procedi' o 'indietro': ");
+        return sc.nextLine();
+    }
+    
+    public void mostra_messaggio(String messaggio)
+    {
+    	//RF13_prenota_posto
+    	//Autori: Rossari, Marisio
+    	
+        Scanner sc = new Scanner(System.in);
+        String conferma;
+        
+        System.out.println(messaggio);
+        System.out.print("Digita 'conferma': ");
+        conferma = sc.nextLine();
+    }
+
 }
--- codice/src/main/java/gestore_lezioni.java
+++ codice/src/main/java/gestore_lezioni.java
@@ -1,10 +1,32 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 class gestore_lezioni
 {
     DB_lezioni db_lez;
 
     public gestore_lezioni(DB_lezioni d1)
     {
-      //autore: Codetta
-      db_lez=d1;
+    	//autore: Codetta
+    	db_lez=d1;
     }
+    
+    public String controllo_data_e_posti(lezione lez)
+    {
+    	//RF13_prenota_posto
+    	//Autori: Rossari, Marisio
+    	
+    	LocalDate today = LocalDate.now();
+    	LocalTime now = LocalTime.now();
+    	
+    	if((lez.giorno.isEqual(today)||lez.giorno.isAfter(today)) && lez.ora_inizio.isAfter(now) && lez.posti_disponibili>0) {
+    		return "ok";
+    	}
+    	if(lez.giorno.isBefore(today) || (lez.ora_inizio.isBefore(now)||lez.ora_inizio.equals(now))) {
+    		return "err_data";
+    	}
+    	//if lez.posti_disponibili == 0
+    	return "err_posti";
+    }
+    
 }
