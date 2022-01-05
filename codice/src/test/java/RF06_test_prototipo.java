f8d5f6be73b0e3b6c500e14c397dff29258712e1
Rosilde Garavoglia <20034672@studenti.uniupo.it>
2022-01-05 16:01:54 +0000

    Replace RF06_test_prototipo.java

--- codice/src/test/java/RF06_test_prototipo.java
+++ codice/src/test/java/RF06_test_prototipo.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Scanner;
 
 /**
  * 
@@ -10,7 +11,18 @@ public class RF06_test_prototipo {
 
 	public static void main(String[] args) {
 		UI_lezione l = new UI_lezione (new UI_avviso(new gestore_avvisi(new DB_avvisi())), new gestore_lezioni(new DB_lezioni()));
-		aula selezionata = l.avvia_seleziona_aula_libera(LocalDate.of(2021, 10 , 3), LocalTime.of(14, 0));
+		System.out.println ("Inserire ora inizio:");
+		Scanner in = new Scanner (System.in);
+		String s = in.nextLine();
+		Integer sh = Integer.parseInt(s);
+		
+		System.out.println ("Inserire ora fine:");
+		in = new Scanner (System.in);
+		s = in.nextLine();
+		Integer eh = Integer.parseInt(s);
+		System.out.println ("");
+		
+		aula selezionata = l.avvia_seleziona_aula_libera(LocalDate.of(2021, 10 , 3), LocalTime.of(sh, 0), LocalTime.of(eh,0));
 		System.out.println("Aula selezionata: " + selezionata.numero);
 	}
 
