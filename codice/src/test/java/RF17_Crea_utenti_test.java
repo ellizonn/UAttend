18aef34c5c7ba7f490c93fe3a8a88bd15363bc46
Mattia La Spisa <20033669@studenti.uniupo.it>
2022-01-06 08:20:13 +0000

    Upload New File

---
+++ codice/src/test/java/RF17_Crea_utenti_test.java
@@ -0,0 +1,46 @@
+package RF17;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RF17_Crea_utenti_test {
+	//RF17: crea utente
+	//autori: La Spisa & Frasson
+	
+	@Test
+	public void controllo_generalità_test() 
+	{
+		DB_utenti d1=new DB_utenti();
+		gestore_utenti g1= new gestore_utenti(d1);
+		indirizzo residenza;
+		residenza=new indirizzo();
+		residenza.CAP=20113;
+		residenza.localita="Milano";
+		residenza.numero=5;
+		residenza.via="Via Roma";
+		assertEquals(true, g1.controllo_generalità("Mario", "Rossi", residenza, "docente", 0));
+		assertEquals(false, g1.controllo_generalità("", "Rossi", residenza, "docente", 0));
+		assertEquals(false, g1.controllo_generalità("Mario", "", residenza, "docente", 0));
+		assertEquals(false, g1.controllo_generalità("Mario", "Rossi", residenza, null, 0));
+	}
+
+	@Test
+	public void genera_credenziali_test() 
+	{
+		 DB_utenti d1=new DB_utenti();
+	     gestore_accessi g1= new gestore_accessi(d1);
+	     assertEquals(true, g1.generazione_credenziali());
+	}
+	
+	
+	@Test
+	public void controlla_scelta_test()
+	{
+		DB_utenti d1=new DB_utenti();
+		gestore_utenti g1= new gestore_utenti(d1);
+		assertEquals(true, g1.controlla_scelta(5));
+		assertEquals(false, g1.controlla_scelta(0));
+	}
+	
+}
