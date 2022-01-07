a773a67511dee3d401c2b175c779af86e0af3ebb
Silvia Masino <20034410@studenti.uniupo.it>
2022-01-07 13:20:39 +0100

    codice

--- codice/src/main/java/UI_ricerca.java
+++ codice/src/main/java/UI_ricerca.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 public class UI_ricerca
 {   
@@ -7,6 +10,10 @@ public class UI_ricerca
     private UI_prenotazione ui_pren;
 
     private gestore_ricerche g_ric;
+    private int scelta_lez;
+    private Scanner sc;
+    private String data_i;
+    private String data_f;
 
     public UI_ricerca(UI_account ui1, UI_lezione ui2, UI_prenotazione ui3, gestore_ricerche g1)
     {	
@@ -16,5 +23,184 @@ public class UI_ricerca
         ui_pren=ui3;
 
         g_ric=g1;
+    }
+
+    public void avvio_ricerca_lezioni(String tipo_utente,int matricola) {
+        //RF12 - Ricerca lezioni per data
+        //autore: Masino, Spina
+
+        boolean esito;
+        ArrayList<lezione> elenco_lezioni;
+        lezione lez;
+        do {
+            this.form_ricerca();
+            elenco_lezioni = g_ric.controllo_data(data_i,data_f);
+            if(elenco_lezioni == null) {
+                mostra_errore_ricerca_lezioni(elenco_lezioni);
+            }
+            else if(elenco_lezioni.size() == 0) {
+                System.out.println("Nessuna lezione nel periodo selezionato.");
+                System.out.println("\n1. Ricerca altre lezioni");
+                System.out.println("2. Esci");
+                int sel_menu;
+                do {
+                    sc = new Scanner(System.in);
+                    sel_menu = sc.nextInt();
+                    if(sel_menu == 1) {
+                        break;
+                    }
+                    else {
+                        exit(0);
+                    }
+                }
+                while(sel_menu < 1 && sel_menu > 2);
+            }
+            else {
+                lez = mostra_elenco_lezioni(elenco_lezioni);
+                do {
+                    if(lez != null) {
+                        this.mostra_menu(tipo_utente);
+                        esito = g_ric.controlla_scelta(scelta_lez, tipo_utente);
+
+                        if (esito==false)
+                            mostra_errore_ricerca_lezioni(elenco_lezioni);
+
+                        if(scelta_lez == 1 && !tipo_utente.equals("studente")) {
+                            // da sostituire con la chiamata del metodo di AVVIO
+                            System.out.println("\nAVVIO visualizza prenotazioni");
+                        }
+                        if(scelta_lez == 2 && tipo_utente.equals("studente")) {
+                            //ui_pren.avvio_prenota_posto(lez,matricola);
+                            System.out.println("\nAVVIO prenota posto");
+                        }
+                        if(scelta_lez == 3 && tipo_utente.equals("staff")) {
+                            // da sostituire con la chiamata del metodo di AVVIO
+                            System.out.println("\nAVVIO cancella lezione");
+                        }
+                        if(scelta_lez == 4 && tipo_utente.equals("staff")) {
+                            // da sostituire con la chiamata del metodo di AVVIO
+                            System.out.println("\nAVVIO modifica lezione");
+                        }
+                    }
+                }
+                while(scelta_lez != 0);
+            }
+        }
+        while(scelta_lez == 0 || elenco_lezioni == null);
+    }
+
+    public void form_ricerca() {
+
+        //RF12 - Ricerca lezioni per data
+        //autore: Masino, Spina
+
+        sc = new Scanner(System.in);
+        System.out.print("\nInserisci data inizio: ");
+        data_i = sc.nextLine();
+        System.out.print("Inserisci data fine: ");
+        data_f = sc.nextLine();
+    }
+
+    public void mostra_errore_ricerca_lezioni(ArrayList<lezione> elenco_lezioni) {
+
+        //RF12 - Ricerca lezioni per data
+        //autore: Masino, Spina
+
+        String conferma;
+        sc = new Scanner(System.in);
+        if(elenco_lezioni == null) {
+            System.err.println("\nERRORE: data passata o formato date errato.");
+        }
+        else {
+            System.err.println("\nERRORE: scelta errata");
+        }
+
+        System.out.print("premi INVIO per confermare");
+        conferma = sc.nextLine();
+    }
+
+    public lezione mostra_elenco_lezioni(ArrayList<lezione> elenco_lezioni) {
+
+        //RF12 - Ricerca lezioni per data
+        //autore: Masino, Spina
+
+        int i = 1;
+        lezione lezione = null;
+
+        //if(elenco_lezioni.size() != 0) {
+        for(lezione lez : elenco_lezioni) {
+            System.out.println("\n-------------------------------------------");
+            System.out.println("Lezione "+i);
+            System.out.println("Giorno: "+lez.giorno);
+            System.out.println("Anno: "+lez.anno);
+            System.out.println("Orario: "+lez.ora_inizio+" "+lez.ora_fine);
+            System.out.println("Corso: "+lez.nome_corso);
+            System.out.println("Docente: "+lez.cognome_docente);
+            System.out.println("Numero Aula: "+lez.numero_aula);
+            System.out.println("Posti disponibili: "+lez.posti_disponibili);
+            System.out.println("-------------------------------------------");
+            i++;
+        }
+        /*}
+        else {
+            System.out.println("Nessuna lezione nel periodo selezionato.");
+            System.out.println("\n1. Ricerca altre lezioni");
+            System.out.println("2. Esci");
+            int sel_menu;
+            do {
+                sc = new Scanner(System.in);
+                sel_menu = sc.nextInt();
+                if(sel_menu == 1) {
+                    return null;
+                }
+                else {
+                    exit(0);
+                }
+            }
+            while(sel_menu < 1 && sel_menu > 2);
+        }*/
+
+        int sel_lez;
+        sc = new Scanner(System.in);
+        i = 1;
+
+        do {
+            System.out.println("Selezionare lezione o 0 per uscire: ");
+            sel_lez = sc.nextInt();
+            if(sel_lez != 0) {
+                for(lezione lez : elenco_lezioni) {
+                    if(i==sel_lez) {
+                        lezione = lez;
+                    }
+                    i++;
+                }
+            }
+            else {
+                exit(0);
+            }
+        }
+        while(sel_lez > elenco_lezioni.size());
+
+        return lezione;
+    }
+
+    public void mostra_menu(String tipo_utente)
+    {
+        //RF12 - Ricerca lezioni per data
+        //autore: Masino, Spina
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\ntipo utente: " + tipo_utente);
+
+
+        if (tipo_utente.equals("docente"))
+            System.out.println("0. Menù principale\n1. Visualizza prenotazioni");
+        if (tipo_utente.equals("studente") )
+            System.out.println("0. Menù principale\n2. Prenota posto\n");
+        if (tipo_utente.equals("staff") )
+            System.out.println("0. Menù principale\n1. Visualizza prenotazioni\n3. Cancella lezione\n4. Modifica lezione");
+
+        System.out.print("\ninserire scelta: ");
+        scelta_lez = sc.nextInt();
     }
 }
