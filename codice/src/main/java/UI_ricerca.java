f557cb95ed3928032ef9ddc5d1ea04f2b83e9dd1
Roberto Malavasi <20033809@studenti.uniupo.it>
2022-01-12 17:47:51 +0100

    correzione codice rf07

--- codice/src/main/java/UI_ricerca.java
+++ codice/src/main/java/UI_ricerca.java
@@ -367,14 +367,19 @@ public class UI_ricerca
 
 		private ArrayList<utente> form_ricerca() {
 			//RF07: ricerca utente
-	    	//autori: Malavasi - Torta
+	    		//autori: Malavasi - Torta
+			int mat=0;
+			String matricola = "";
+			String cognome = "";
 			Scanner sc = new Scanner(System.in);
 			System.out.println("\nInserisci matricola: ");
-			int matricola = sc.nextInt();
+			matricola = sc.nextLine();
+			if(!matricola.equals(""))
+				mat=Integer.parseInt(matricola);
 			System.out.println("\nInserisci cognome: ");
-			String cognome = sc.next();
+			cognome = sc.nextLine();
 			System.out.println("\nAvvio ricerca utente\n");
-			ArrayList<utente> u = g_ric.verifica_parametri(matricola, cognome);
+			ArrayList<utente> u = g_ric.verifica_parametri(mat, cognome);
 			return u;
 		}
 
