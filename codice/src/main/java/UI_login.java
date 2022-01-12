9f364c3c776004eba897bbf79adf675955a3c71b
Martina Balossino <20034043@studenti.uniupo.it>
2022-01-12 09:24:52 +0000

    Update UI_login.java

--- codice/src/main/java/UI_login.java
+++ codice/src/main/java/UI_login.java
@@ -95,7 +95,7 @@ public class UI_login
                         }
                         if (scelta==4 && tipo_utente.equals("studente"))
                             System.out.println("\nAVVIO visualizza prenotazioni");
-                            VisualizzaPrenotazioni.visualizzaStudente();
+                            ui_pren.visualizzaStudente();
 	
                         if (scelta==5 && tipo_utente.equals("staff")) {
                             System.out.println("\nAVVIO crea corso");
