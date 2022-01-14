ecc54ee7736b0c2205c898d473c7055668a5a5f9
Martina Balossino <20034043@studenti.uniupo.it>
2022-01-14 22:54:56 +0000

    Update RF09_visualizza_prenotazioni_test.java

--- codice/src/test/java/RF09_visualizza_prenotazioni_test.java
+++ codice/src/test/java/RF09_visualizza_prenotazioni_test.java
@@ -10,14 +10,28 @@ public class RF09_visualizza_prenotazioni_test {
 		gestore_lezioni g_lez = new gestore_lezioni(db_lez);
 		UI_prenotazione ui_pren = new UI_prenotazione(g_lez);
 		
-		/* NON COMPILA:
-		System.out.print("//DOCENTE//\n");
-		ui_pren.visualizza_prenotazioni(UI_prenotazione.utente.DOCENTE);
-		System.out.print("//STUDENTE//\n");
-		ui_pren.visualizza_prenotazioni(UI_prenotazione.utente.STUDENTE);
-		System.out.print("//STAFF//\n");
-		ui_pren.visualizza_prenotazioni(UI_prenotazione.utente.STAFF);
-		*/
+		System.out.print("\n//STAFF LEZIONI//\n");
+		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Codetta", "Staff");
+		System.out.print("\n//STAFF LEZIONI//\n");
+		ui_pren.visualizza_prenotaz_lez("Sistemi_Operativi_1", "Cerotti", "Staff");
+		System.out.print("\n//STAFF LEZIONI//\n");
+		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Cerotti", "Staff");
+		
+		System.out.print("\n//DOCENTE LEZIONI//\n");
+		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Codetta", "Docente");
+		System.out.print("\n//DOCENTE LEZIONI//\n");
+		ui_pren.visualizza_prenotaz_lez("Sistemi_Operativi_1", "Cerotti", "Docente");
+		System.out.print("\n//DOCENTE LEZIONI//\n");
+		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Cerotti", "Docente");
+		
+		System.out.print("\n//STUDENTE LEZIONI//\n");
+		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Codetta", "Studente");
+		
+		System.out.print("\n//STUDENTE//\n");
+		ui_pren.visualizza_prenotaz_stud(100003, "Studente");
+		System.out.print("\n//STUDENTE//\n");
+		ui_pren.visualizza_prenotaz_stud(100003, "Docente");
+		
 	}
 
 }
