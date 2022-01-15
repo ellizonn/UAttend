560d6db77b64f442dbbd5acb4b929cfe6bd9dced
Martina Balossino <20034043@studenti.uniupo.it>
2022-01-15 20:29:37 +0000

    Update RF09_visualizza_prenotazioni_test.java

--- codice/src/test/java/RF09_visualizza_prenotazioni_test.java
+++ codice/src/test/java/RF09_visualizza_prenotazioni_test.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import org.junit.Test;
 
@@ -10,22 +12,52 @@ public class RF09_visualizza_prenotazioni_test {
 		gestore_lezioni g_lez = new gestore_lezioni(db_lez);
 		UI_prenotazione ui_pren = new UI_prenotazione(g_lez);
 		
+		lezione lez1=new lezione();
+		lez1.nome_corso=("Programmazione_1");
+		lez1.cognome_docente=("Codetta");
+		lez1.numero_aula=(1);
+		lez1.numero_aula=(7);
+		lez1.posti_disponibili=(45);
+		lez1.giorno=(LocalDate.of(2021, 10, 1));
+		lez1.ora_inizio=(LocalTime.of(9,0));
+		lez1.ora_fine=(LocalTime.of(13,0));
+		
+		lezione lez2=new lezione();
+		lez2.nome_corso=("Ingegneria_del_Software");
+		lez2.cognome_docente=("Codetta");
+		lez2.numero_aula=(3);
+		lez2.numero_aula=(11);
+		lez2.posti_disponibili=(24);
+		lez2.giorno=(LocalDate.of(2021, 1, 17));
+		lez2.ora_inizio=(LocalTime.of(9,0));
+		lez2.ora_fine=(LocalTime.of(13,0));
+		
+		lezione lez3=new lezione();
+		lez3.nome_corso=("Sistemi_Operativi_1");
+		lez3.cognome_docente=("Cerotti");
+		lez3.numero_aula=(2);
+		lez3.numero_aula=(4);
+		lez3.posti_disponibili=(30);
+		lez3.giorno=(LocalDate.of(2021, 10, 2));
+		lez3.ora_inizio=(LocalTime.of(11,0));
+		lez3.ora_fine=(LocalTime.of(13,0));
+		
 		System.out.print("\n//STAFF LEZIONI//\n");
-		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Codetta", "Staff");
+		ui_pren.visualizza_prenotaz_lez(lez1, "Staff");
 		System.out.print("\n//STAFF LEZIONI//\n");
-		ui_pren.visualizza_prenotaz_lez("Sistemi_Operativi_1", "Cerotti", "Staff");
+		ui_pren.visualizza_prenotaz_lez(lez3, "Staff");
 		System.out.print("\n//STAFF LEZIONI//\n");
-		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Cerotti", "Staff");
+		ui_pren.visualizza_prenotaz_lez(lez2, "Staff");
 		
 		System.out.print("\n//DOCENTE LEZIONI//\n");
-		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Codetta", "Docente");
+		ui_pren.visualizza_prenotaz_lez(lez1, "Docente");
 		System.out.print("\n//DOCENTE LEZIONI//\n");
-		ui_pren.visualizza_prenotaz_lez("Sistemi_Operativi_1", "Cerotti", "Docente");
+		ui_pren.visualizza_prenotaz_lez(lez3, "Docente");
 		System.out.print("\n//DOCENTE LEZIONI//\n");
-		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Cerotti", "Docente");
+		ui_pren.visualizza_prenotaz_lez(lez2, "Docente");
 		
 		System.out.print("\n//STUDENTE LEZIONI//\n");
-		ui_pren.visualizza_prenotaz_lez("Programmazione_1", "Codetta", "Studente");
+		ui_pren.visualizza_prenotaz_lez(lez1, "Studente");
 		
 		System.out.print("\n//STUDENTE//\n");
 		ui_pren.visualizza_prenotaz_stud(100003, "Studente");
